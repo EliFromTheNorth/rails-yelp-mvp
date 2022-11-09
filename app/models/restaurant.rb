040e451e773468c640fcd7b6c41cf5ad8c7cda34
Eliska Knop Kostkova <e.knopkostkova@gmail.com>
2022-11-09 13:11:46 +0800

    schema and validations created, now needs to fix range or ratings and test

---
+++ app/models/restaurant.rb
@@ -0,0 +1,8 @@
+class Restaurant < ApplicationRecord
+  has many :reviews, dependent: :destroy
+
+  CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian"]
+  validates :name, presence: true
+  validates :adress, presence: true
+  validates :category, presence: true, inclusion: {in: CATEGORIES}
+end
